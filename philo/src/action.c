d8b9268b352dc0c432bcd61e06bbde23f1984090
Alexis Le Roux <ale-roux@k2r4p10.42mulhouse.fr>
2023-07-26 23:18:50 +0200

    corrected

--- philo/src/action.c
+++ philo/src/action.c
@@ -6,7 +6,7 @@
 /*   By: ale-roux <ale-roux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 01:38:57 by ale-roux          #+#    #+#             */
-/*   Updated: 2023/07/26 23:12:29 by ale-roux         ###   ########.fr       */
+/*   Updated: 2023/07/26 23:18:21 by ale-roux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,19 @@
 void	talk(t_philo *philo, int id, long long time_in_ms, int action)
 {
 	pthread_mutex_lock(&philo->utils.talk);
-	if (action == 0)
-		printf("%lld %d is thinking\n", time_in_ms, id);
-	else if (action == 1)
-		printf("%lld %d has taken a fork\n", time_in_ms, id);
-	else if (action == 2)
-		printf("%lld %d is eating\n", time_in_ms, id);
-	else if (action == 3)
-		printf("%lld %d is sleeping\n", time_in_ms, id);
-	else if (action == 4)
-		printf("%lld %d died\n", time_in_ms, id);
+	if (philo->isdead == false)
+	{
+		if (action == 0)
+			printf("%lld %d is thinking\n", time_in_ms, id);
+		else if (action == 1)
+			printf("%lld %d has taken a fork\n", time_in_ms, id);
+		else if (action == 2)
+			printf("%lld %d is eating\n", time_in_ms, id);
+		else if (action == 3)
+			printf("%lld %d is sleeping\n", time_in_ms, id);
+		else if (action == 4)
+			printf("%lld %d died\n", time_in_ms, id);
+	}
 	pthread_mutex_unlock(&philo->utils.talk);
 }
 
